5d1925e137f2aaadd2d42f1cb14a7008359e230e
gathei-iraki <slyiraki@gmail.com>
2020-02-24 12:36:35 +0300

    Added a java script function to display error message incase data is not entered

---
+++ js/dela.js
@@ -0,0 +1,9 @@
+var check = function () {
+    var name = document.getElementById("name").value;
+     var email = document.getElementById("email").value;
+}
+if (name== "" ){
+    alert("Please enter your details in the provided field");
+  } else if (email == ""){
+    alert("Please enter a valid email in the field provided");
+  }
