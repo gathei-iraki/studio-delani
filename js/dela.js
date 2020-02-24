518a9ad5e684f3c8f560e971f885273426af1964
gathei-iraki <slyiraki@gmail.com>
2020-02-24 14:44:01 +0300

    Removed the java function

--- js/dela.js
+++
@@ -1,9 +0,0 @@
-var check = function () {
-    var name = document.getElementById("name").value;
-     var email = document.getElementById("email").value;
-}
-if (name== "" ){
-    alert("Please enter your details in the provided field");
-  } else if (email == ""){
-    alert("Please enter a valid email in the field provided");
-  }
