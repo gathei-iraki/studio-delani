2244aa3ad85ae46609de07ff2f9fec78823003dd
gathei-iraki <slyiraki@gmail.com>
2020-02-24 17:56:33 +0300

    Added alert

---
+++ js/dela.js
@@ -0,0 +1,7 @@
+function getData() {
+    var a = document.getElementById("name").value;
+    alert("Hey, " + a + ". We got your message. Thanks for reaching out to us.")
+    event.preventDefault()
+
+
+  }
