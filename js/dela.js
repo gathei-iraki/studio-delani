822ec0afc86e44c9caef4b79f9087441e1375157
gathei-iraki <slyiraki@gmail.com>
2020-02-25 10:05:13 +0300

    Added toggle function to the java

--- js/dela.js
+++ js/dela.js
@@ -5,3 +5,19 @@ function getData() {
 
 
   }
+  $().ready(function () {
+    $("#pen-image").click(function () {
+      $("#pen-whole").toggle();
+      $("#pen-word").toggle();
+    });
+
+    $("#develop").click(function () {
+      $("#develop-1").toggle();
+      $("#develop-2").toggle();
+    });
+
+    $("#product").click(function () {
+      $("#product-1").toggle();
+      $("#product-2").toggle();
+    });
+  });
